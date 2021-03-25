3f318e46e34933b99429614ccc7f79073f1b75d9
Dolgoulsky <a.v.dolgoulskii@gmail.com>
2021-03-26 00:35:10 +0600

    first version

---
+++ variables.dart
@@ -0,0 +1,22 @@
+main() {
+  int year = 365;
+  double hours = 23.56;
+  var yourName = "Alex";
+  String name = "Sasha";
+  final firsName = "Alexander";
+  const lastName = "Dolgoulsky";
+  bool hiIsAMan = true;
+  dynamic yearOld = "39";
+
+  List array = [
+    year,
+    hours,
+    yourName,
+    name,
+    firsName,
+    lastName,
+    hiIsAMan,
+    yearOld
+  ];
+  print(array);
+}
